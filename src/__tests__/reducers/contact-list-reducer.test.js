432b1ab91b875ce8af1767dd52dd9044f875609f
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-14 16:01:18 -0700

    write and pass second test

--- src/__tests__/reducers/contact-list-reducer.test.js
+++ src/__tests__/reducers/contact-list-reducer.test.js
@@ -1,7 +1,36 @@
 import contactListReducer from '../../reducers/contact-list-reducer';
 
 describe('contactListReducer', () => {
+
+  let action;
+  const contactData = {
+    name: "Aaron",
+    phone: "702-666-6666",
+    email: "aaron@aaron.aaron",
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed onto the reducer', () => {
     expect(contactListReducer({}, { type: null })).toEqual({});
+  })
+
+  test('Should successfully add new contact data to mainContactList', () => {
+    const { name, phone, email, id } = contactData;
+    action = {
+      type: "ADD_CONTACT",
+      name: name,
+      phone: phone,
+      email: email, 
+      id: id
+    }
+
+    expect(contactListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        phone: phone,
+        email: email,
+        id: id
+      }
+    })
   })
 })
--- src/reducers/contact-list-reducer.js
+++ src/reducers/contact-list-reducer.js
@@ -1,5 +1,18 @@
 const reducer = (state = {}, action) => {
-  return state;
-}
+  const { name, phone, email, id } = action;
+  switch (action.type) {
+    case "ADD_CONTACT":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          phone: phone,
+          email: email,
+          id: id,
+        },
+      });
+    default:
+      return state;
+  }
+};
 
 export default reducer;
