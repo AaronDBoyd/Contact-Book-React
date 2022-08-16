04e74a4d6739af1d4e7c5c077cac9a4db0c8966e
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-16 13:08:49 -0700

    write and pass fourth and fifth test

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,24 +1,51 @@
-import rootReducer from '../../reducers/index';
-import { createStore } from 'redux';
-import formVisibleReducer from '../../reducers/form-visible-reducer';
-import contactListReducer from '../../reducers/contact-list-reducer';
+import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
+import contactListReducer from "../../reducers/contact-list-reducer";
 
 let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
-
-  test('Should return default state if no action type is recognized', () => {
+  test("Should return default state if no action type is recognized", () => {
     expect(rootReducer({}, { type: null })).toEqual({
       mainContactList: {},
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
     });
   });
 
-  test('Check that initial state of contactListReducer matches root reducer', () => {
-    expect(store.getState().mainContactList).toEqual(contactListReducer(undefined, {type: null}) )
-  })
+  test("Check that initial state of contactListReducer matches root reducer", () => {
+    expect(store.getState().mainContactList).toEqual(
+      contactListReducer(undefined, { type: null })
+    );
+  });
 
-  test('Check that initial state if formVisibleReducer matches root reducer', () => {
-    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }))
-  })
+  test("Check that initial state if formVisibleReducer matches root reducer", () => {
+    expect(store.getState().formVisibleOnPage).toEqual(
+      formVisibleReducer(undefined, { type: null })
+    );
+  });
+
+  test("Check that ADD_CONTACT action woeks for contactListReducer and root reducer", () => {
+    const action = {
+      type: "ADD_CONTACT",
+      name: "aaron",
+      phone: "123-456-7890",
+      email: "aaron@aaron.aaron",
+      id: 1,
+    };
+    store.dispatch(action);
+    expect(store.getState().mainContactList).toEqual(
+      contactListReducer(undefined, action)
+    );
+  });
+
+  test("Check that TOGGLE_FORM action works for formVisibleReducer and root reducer", () => {
+    const action = {
+      type: "TOGGEL_FORM",
+    };
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(
+      formVisibleReducer(undefined, action)
+    );
+  });
 });
