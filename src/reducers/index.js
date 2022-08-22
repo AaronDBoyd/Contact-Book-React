b23a733eafc2d67bb1db0c43c4541770e79e394f
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-22 15:13:23 -0700

    add firebase reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import formVisibleReducer from './form-visible-reducer';
 import contactListReducer from './contact-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainContactList: contactListReducer
+  mainContactList: contactListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
