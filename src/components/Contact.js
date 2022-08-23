005b1fd48be68af8322152595be5cbb9fc3164e8
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-22 17:43:58 -0700

    display contacts from firestore

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ function Contact(props) {
   return (
     <React.Fragment>
       <div onClick = {() => props.whenContactClicked(props.id)}>
+        <hr/>
         <h3>Name: {props.name}</h3>
         <h4>Phone: {props.phone}</h4>
         <h4>Email: {props.email}</h4>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,26 +1,39 @@
 import React from "react";
 import Contact from "./Contact";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
 
 export default function ContactList(props) {
-  return (
-    <React.Fragment>
-      {Object.values(props.contactList).map((contact, index) => (
-        <Contact
-          whenContactClicked = { props.onContactSelection }
-          name={contact.name}
-          phone={contact.phone}
-          email={contact.email}
-          id= {contact.id}
-          key={contact.id}
-        />
-      ))}
-    </React.Fragment>
-  );
+  useFirestoreConnect([{ collection: "contacts" }]);
+
+  const contacts = useSelector((state) => state.firestore.ordered.contacts);
+
+  if (isLoaded(contacts)) {
+    return (
+      <React.Fragment>
+        {contacts.map((contact) => (
+          <Contact
+            whenContactClicked={props.onContactSelection}
+            name={contact.name}
+            phone={contact.phone}
+            email={contact.email}
+            id={contact.id}
+            key={contact.id}
+          />
+        ))}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    );
+  }
 }
 
 ContactList.propTypes = {
-  contactList: PropTypes.object,
-  onContactSelection: PropTypes.func
+  // contactList: PropTypes.object,
+  onContactSelection: PropTypes.func,
 };
