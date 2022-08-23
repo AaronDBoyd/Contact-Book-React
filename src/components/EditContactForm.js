93070197d27129865095b9b9b822be218826ba1d
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-22 23:43:30 -0700

    update contacts using useFirestore hooks

--- src/components/EditContactForm.js
+++ src/components/EditContactForm.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReusableForm from './ReusableForm'
+import { useFirestore } from 'react-redux-firebase';
 
 function EditContactForm(props) {
   const { contact } = props;
+  const firestore = useFirestore();
 
   function handleEditContactFormSubmission(event) {
     event.preventDefault();
-    props.onEditContact({
+    props.onEditContact();
+    const propertiesToUpdate = {
       name: event.target.name.value,
       phone: event.target.phone.value,
       email: event.target.email.value,
       id: contact.id
-    })
+    }
+    return firestore.update({collection: 'contacts', doc: contact.id }, propertiesToUpdate)
   }
 
   return (
